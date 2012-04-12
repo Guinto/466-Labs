35f36c81b641e47b2132924e861c0adc0860e4b3
Trent Ellingsen <trentellingsen@gmail.com>
2012-04-12 00:25:12 -0700

    added some get functions for DTN

--- SupervisedLearning/DecisionTreeNode.java
+++ SupervisedLearning/DecisionTreeNode.java
@@ -6,5 +6,18 @@ public class DecisionTreeNode {
    
 	public DecisionTreeNode(int value) {
 	   this.value = value;
+	   children = new ArrayList<DecisionTreeNode>();
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public ArrayList<DecisionTreeNode> getChildren() {
+		return children;
+	}
+	
+	public void setChildren(ArrayList<DecisionTreeNode> children) {
+		this.children = children;
 	}
 }
