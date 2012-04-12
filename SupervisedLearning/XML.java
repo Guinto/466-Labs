391121f5b1ac12c095495ee54a28981aa44b93e4
Trent Ellingsen <trentellingsen@gmail.com>
2012-04-11 18:19:40 -0700

    xml now can read the domain file and prints out its contents

--- SupervisedLearning/XML.java
+++ SupervisedLearning/XML.java
@@ -1,58 +1,52 @@
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 import java.io.File;
-public class XML 
-{
-   public static void main( String[] args )
-   {
-      try {
+import java.io.IOException;
 
-         File fXmlFile = new File("domain.xml");
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+import javax.xml.parsers.*;
+
+public class XML {
+
+	 private DocumentBuilder builder;
+	 
+	 public static void main(String args[]) {
+		 new XML();
+	 }
+	 
+	 public XML() {
+		 try {
+			print("data/domain.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	 }
+
+	 public void print(String fileName) throws ParserConfigurationException, SAXException, IOException {
+         File fXmlFile = new File(fileName);
          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(fXmlFile);
-         doc.getDocumentElement().normalize();
+         Document document = dBuilder.parse(fXmlFile);
+         document.getDocumentElement().normalize();
+         
+		 NodeList nodes_i = document.getDocumentElement().getChildNodes();
 
-         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-         NodeList nList = doc.getElementsByTagName("Category");
-         System.out.println(nList.getLength());
-         System.out.println("-----------------------");
-
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-
-            Node nNode = nList.item(temp);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-               NodeList cList = nNode.getChildNodes();
-               for(int j = 0; j < cList.getLength(); j++) {
-                  System.out.println(cList.getLength());
-                  System.out.println("stuff " + cList.item(j).getNodeValue());
-
-           //    Element eElement = (Element) cNode;
-             //  System.out.println(cNode.getTagName());
-               
-        //       System.out.println("First Name : " + cNode.getNodeValue());
-            //   System.out.println("Last Name : " + getTagValue("group", eElement));
-              // System.out.println("Nick Name : " + getTagValue("nickname", eElement));
-               //System.out.println("Salary : " + getTagValue("salary", eElement));
-               }
-            }
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-
-   
-   private static String getTagValue(String sTag, Element eElement) {
-    NodeList nlList = eElement.getElementsByTagName(sTag);
-  
-         Node nValue = (Node) nlList.item(0);
-  
-    return nValue.getNodeValue();
-   }
-  
+		 for (int i = 0; i < nodes_i.getLength(); i++) {
+			 Node variableNode = nodes_i.item(i);
+			 if (variableNode.getNodeType() == Node.ELEMENT_NODE && ((Element) variableNode).getTagName().equals("variable")) {
+				 Element variable = (Element) variableNode;
+				 System.out.println(variable.getAttribute("name"));
+				 NodeList groups = variable.getChildNodes();
+				 for (int j = 0; j < groups.getLength(); j++) {
+					 Node groupNode = groups.item(j);
+					 if (groupNode.getNodeType() == Node.ELEMENT_NODE && ((Element) groupNode).getTagName().equals("group")) {
+						 Element group = (Element) groupNode;
+						 System.out.println(group.getAttribute("name") + " : " + group.getAttribute("p"));
+					 }
+				 }
+				 System.out.println();
+			 }
+		 }
+		 return;
+	 }
 }
