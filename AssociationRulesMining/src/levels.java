7c372a0bb44ef0036d27910ecf577ffd75c4e2bd
Thomas Wong <tw0ng@hotmail.com>
2012-04-19 09:54:06 -0700

    asdf

---
+++ AssociationRulesMining/src/levels.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+
+
+public class levels {
+   ArrayList<ArrayList<Node>> levels;
+
+   public levels() {
+      this.levels = new ArrayList<ArrayList<Node>>();
+   }
+
+   public ArrayList<Node> findUnions(ArrayList<Node> prev) {
+      ArrayList<Node> next = new ArrayList<Node>();
+      Node temp;
+      for(int i = 0; i < prev.size(); i++) {
+         for(int j = i; j < prev.size(); j++) {
+            if(almostEqual(prev.get(i), prev.get(j))) {
+               temp = new Node(unionName(prev.get(i), prev.get(j)), prev.get(i), prev.get(j));
+               if(next.contains(temp)) {
+                  next.get(next.indexOf(temp)).children.add(prev.get(j));
+               }
+               else {
+                  next.add(new Node(unionName(prev.get(i), prev.get(j)), prev.get(i), prev.get(j)));
+               }
+            }
+         }
+      }
+      return next;
+   }
+
+   public static boolean almostEqual(Node first, Node second) {
+      int count = 0;
+      for(Integer base: first.name) {
+         if(second.name.contains(base)) {
+            count++;
+         }
+      }
+      if(count == first.name.size()-1)
+         return true;
+      return false;
+   }
+
+   public static ArrayList<Integer> unionName(Node first, Node second) {
+      ArrayList<Integer> ans = new ArrayList<Integer>();
+      for(Integer base: first.name) {
+         ans.add(base);
+      }
+      for(Integer base: second.name) {
+         if(!ans.contains(base))
+            ans.add(base);
+      }
+      return ans;
+   }
+
+   private class Node {
+      ArrayList<Integer> name;
+      ArrayList<Node> children;
+      Boolean skyline;
+      public Node(ArrayList<Integer> name, Node child1, Node child2) {
+         this.name = name;
+         this.children = new ArrayList<Node>();
+         this.children.add(child1);
+         this.children.add(child2);
+         this.skyline = false;
+      }
+
+      public boolean equals (Object x) {
+         int count = 0;
+         for(Integer check: this.name) {
+            if(!((Node) x).children.contains(check)) {
+               return false;
+            }
+         }
+         if(((Node) x).children.size() != count)
+            return false;
+         return true;
+      }
+   }
+}
