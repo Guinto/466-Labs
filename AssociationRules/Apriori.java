361b95e8d21bac29c343f2dc2237d0028ded78da
Thomas Wong <tw0ng@hotmail.com>
2012-04-21 15:14:21 -0700

    transferred private node class to it's own class and added minSup and confidence to apriori

--- AssociationRules/Apriori.java
+++ AssociationRules/Apriori.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 
 public class Apriori {
 
-	
+   public double minSup(ArrayList<Node> items, CSV data) {
+      ArrayList<Integer> names = new ArrayList<Integer>();
+      ArrayList<Integer> check = new ArrayList<Integer>();
+      
+      for(int i = 0; i < items.size(); i++) {
+         for(int j = 0; j < items.get(i).name.size(); j++) {
+            if(!names.contains(items.get(i).name.get(j))) {
+               names.add(items.get(i).name.get(j));
+            }
+         }
+      }
+      
+      check.addAll(data.sets.get(names.get(0)));
+      
+      for(int i = 1; i < names.size(); i++) {
+         for(int j = 0; j < check.size(); j++) {
+            if(!data.sets.get(names.get(i)).contains(check.get(j))) {
+               check.remove(check.get(j));
+            }
+         }
+      }
+      
+      return (double)check.size()/data.sets.size();
+   }
+   
+   public double confidence(ArrayList<Node> items, ArrayList<Node> items2, CSV data) {
+      return minSup(items, data)/minSup(items2, data);
+   }
 }
--- AssociationRules/levels.java
+++ AssociationRules/levels.java
@@ -62,34 +62,5 @@ public class levels {
       return ans;
    }
 
-   private class Node {
-      ArrayList<Integer> name;
-      ArrayList<Node> children;
-      Boolean skyline;
-      public Node(ArrayList<Integer> name, Node child1, Node child2) {
-         this.name = name;
-         this.children = new ArrayList<Node>();
-         this.children.add(child1);
-         this.children.add(child2);
-         this.skyline = false;
-      }
-      
-      public Node(ArrayList<Integer> name) {
-         this.name = name;
-         this.children = null;
-         this.skyline = true;
-      }
 
-      public boolean equals (Object x) {
-         int count = 0;
-         for(Integer check: this.name) {
-            if(!((Node) x).children.contains(check)) {
-               return false;
-            }
-         }
-         if(((Node) x).children.size() != count)
-            return false;
-         return true;
-      }
-   }
 }
