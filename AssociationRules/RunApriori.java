d6e11a169d126b7ff100c3492a14be24aa7e8f08
Trent Ellingsen <trentellingsen@gmail.com>
2012-04-24 19:01:05 -0700

    set up the arguments so it works correctly

--- AssociationRules/RunApriori.java
+++ AssociationRules/RunApriori.java
@@ -4,8 +4,7 @@ public class RunApriori {
    public static void main(String[] args) {
       CSV data = null;
       if (args.length >= 0 && args.length < 4) {
-         //data = new CSV(args[0]);
-         data = new CSV("data/factors/factor_baskets_sparse.csv", 222);
+         data = new CSV(args[0]);
       } 
       else if(args.length >= 4) {
          data = new CSV(args[0], 0);
@@ -15,7 +14,6 @@ public class RunApriori {
          System.exit(1);
       }
       Apriori apriori = new Apriori();
-      //apriori.run(data, Double.parseDouble(args[1]), Double.parseDouble(args[2]));
-      apriori.run(data, 97, 90);
+      apriori.run(data, Double.parseDouble(args[1]), Double.parseDouble(args[2]));
    }
 }
