32e58bb57565b756c88bd6796295cd19a310b59c
Trent Ellingsen <trentellingsen@gmail.com>
2012-05-02 16:07:02 -0700

    added beginning functionality

--- Clustering/src/KMeans.java
+++ Clustering/src/KMeans.java
@@ -1,18 +1,39 @@
+import java.util.ArrayList;
+
 
 public class KMeans {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		CSV data = null;
 		if (args.length != 2) {
 			data = new CSV("lab4data/birth_death_rate.csv");
+			System.out.println(data);
+			
+			new KMeans(data, Integer.parseInt(args[1]));
 		} else {
 			System.err.println("Usage: KMeans <fileName> <k>");
 			System.exit(1);
 		}
+	}
+	
+	public KMeans(CSV data, int numClusters) {
+		ArrayList<Integer> centroids = selectInitialCentroids(data.vectors.size(), numClusters);
+		ArrayList<Vector> clusters = new ArrayList<Vector>();
+		int[] numPointsInCluster = new int[numClusters];
 		
-		System.out.println(data);
+		for (int i = 0; i < numClusters; i++) {
+			numPointsInCluster[i] = 0;
+		}
+		
+		
+	}
+	
+	private ArrayList<Integer> selectInitialCentroids(int dataSize, int numClusters) {
+		ArrayList<Integer> centroids = new ArrayList<Integer>();
+		for (int i = 0; i < numClusters; i++) {
+			centroids.add((int)Math.floor(Math.random() * dataSize));
+		}
+		
+		return centroids;
 	}
 }
