4ab4301ced0ce6b9660cf3221f0fe25371e19e44
Thomas Wong <tw0ng@hotmail.com>
2012-05-02 01:34:29 -0700

    did testing

--- AssociationRules/RunApriori.java
+++ AssociationRules/RunApriori.java
@@ -4,7 +4,7 @@ public class RunApriori {
    public static void main(String[] args) {
       CSV data = null;
       if (args.length >= 0 && args.length < 4) {
-         data = new CSV(args[0]);
+         data = new CSV("data/75000/75000-out1.csv");
       } 
       else if(args.length >= 4) {
          data = new CSV(args[0], 0);
@@ -14,6 +14,7 @@ public class RunApriori {
          System.exit(1);
       }
       Apriori apriori = new Apriori();
-      apriori.run(data, Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+      //apriori.run(data, Double.parseDouble(args[1]), Double.parseDouble(args[2]));
+      apriori.run(data, 3, 90);
    }
 }
