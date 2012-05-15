d44f8b3835bd850440f6d833af74f8acc20079a4
Trent Ellingsen <trentellingsen@gmail.com>
2012-05-14 22:18:05 -0700

    added similarity function and weighted sum

--- CollaborativeFilteringAndRecommenderSystem/src/Filter.java
+++ CollaborativeFilteringAndRecommenderSystem/src/Filter.java
@@ -9,7 +9,8 @@ public class Filter {
 	public Filter(CSV data) {
 		this.data = data;
 		userRatings = new ArrayList<UserRating>();
-		System.out.println("prediction for 0, 10: " + meanUtility(0, 10));
+		System.out.println("mean utility prediction for 0, 10: " + meanUtility(0, 10));
+		System.out.println("weighted sum prediction for 0, 10: " + weightedSum(0, 10));
 	}
 	
 	public float meanUtility(int user, int joke) {
@@ -36,6 +37,49 @@ public class Filter {
 		return total / notNullPeople;
 	}
 	
+	public float weightedSum(int user, int joke) {
+		float total = 0;
+		UserRating a = new UserRating(user, data.getVectors().get(user));
+		
+		for (int i = 0; i < data.getVectors().size(); i++) {
+			if (i != user && data.getVectors().get(i).get(joke) != 99) {
+				UserRating b = new UserRating(i, data.getVectors().get(i));
+				total += similarity(a, b) * data.getVectors().get(i).get(joke);
+			}
+		}
+		return normalization(user, joke) + total;
+	}
+	
+	private float normalization(int user, int joke) {
+		float total = 0;
+		UserRating a = new UserRating(user, data.getVectors().get(user));
+		
+		for (int i = 0; i < data.getVectors().size(); i++) {
+			if (i != user && data.getVectors().get(i).get(joke) != 99) {
+				UserRating b = new UserRating(i, data.getVectors().get(i));
+				total += similarity(a, b);
+			}
+		}
+		return 1 / Math.abs(total);
+	}
+		
+	private float similarity(UserRating a, UserRating b) {
+		float top = 0, bottom = 0;
+		float meanForA = avgRatingForUser(a.id);
+		float meanForB = avgRatingForUser(b.id);
+		
+		for (int i = 0; i < a.ratings.size(); i++) {
+			if (a.ratings.get(i) == 99 && b.ratings.get(i) == 99) continue;
+			top += ((a.ratings.get(i) - meanForA) * (b.ratings.get(i) - meanForB));
+		}
+		for (int i = 0; i < a.ratings.size(); i++) {
+			if (a.ratings.get(i) == 99 && b.ratings.get(i) == 99) continue;
+			bottom += Math.sqrt((Math.pow((a.ratings.get(i) - meanForA), 2) * Math.pow((b.ratings.get(i) - meanForB), 2)));
+		}
+		
+		return top / bottom;
+	}
+	
 	public float avgRatingForUser(int user) {
 		float total = 0;
 		for (int i = 0; i < data.getVectors().get(user).size(); i++) {
@@ -48,6 +92,11 @@ public class Filter {
 	
 	class UserRating {
 		public int id;
-		public ArrayList<Float> ratings = new ArrayList<Float>();
+		public Vector ratings;
+		
+		public UserRating(int id, Vector ratings) {
+			this.id = id;
+			this.ratings = ratings;
+		}
 	}
 }
