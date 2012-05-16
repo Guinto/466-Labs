bfda2559910645d05c4540ea9d5936c4a7a91b7b
Thomas Wong <tw0ng@hotmail.com>
2012-05-16 16:51:39 -0700

    updated lab and added 100 and 500 to report

--- CollaborativeFilteringAndRecommenderSystem/src/EvaluateCFRandom.java
+++ CollaborativeFilteringAndRecommenderSystem/src/EvaluateCFRandom.java
@@ -4,9 +4,9 @@ public class EvaluateCFRandom {
 	
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			new Filter(new CSV("jester-data-1.csv"), 0, 100);
-			new Filter(new CSV("jester-data-1.csv"), 1, 100);
-			new Filter(new CSV("jester-data-1.csv"), 2, 100);
+			new Filter(new CSV("jester-data-1.csv"), 0, 500);
+			new Filter(new CSV("jester-data-1.csv"), 1, 500);
+			new Filter(new CSV("jester-data-1.csv"), 2, 500);
 		} else {
 			System.err.println("Usage: EvaluateCFRandom Method Size");
 			System.exit(1);
