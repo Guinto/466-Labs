ec65ae3ef3411ff6513194cbc2ba9434fa93656e
Trent Ellingsen <trentellingsen@gmail.com>
2012-05-30 19:07:38 -0700

    started the project for pageRank

---
+++ PageRank/src/pageRank.java
@@ -0,0 +1,18 @@
+
+public class pageRank {
+
+	public static void main(String[] args) {
+	   	if (args.length == 0) {
+	   		new pageRank();
+		} else {
+			System.err.println("Usage: HClustering <fileName> [<threshold>]");
+			System.exit(1);
+		}
+
+	}
+	
+	public pageRank() {
+		
+	}
+
+}
