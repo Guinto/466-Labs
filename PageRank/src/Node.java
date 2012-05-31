ce672dffd0102a5e86a78a6dbfb1fc78f070f9af
Thomas Wong <tw0ng@hotmail.com>
2012-05-31 03:39:43 -0700

    Finished pagerank except directed

--- PageRank/src/Node.java
+++ PageRank/src/Node.java
@@ -1,9 +1,9 @@
 
-public class Node {
+public class Node implements Comparable{
    private String name;
-   private int value;
+   private double value;
    
-   public Node(String name, int value) {
+   public Node(String name, double value) {
       this.name = name;
       this.value = value;
    }
@@ -12,7 +12,27 @@ public class Node {
       return name;
    }
 
-   public int getValue() {
+   public double getValue() {
       return value;
    }
+
+   
+   public int compareTo(Object arg0) {
+      if(this.getValue() > ((Node) arg0).getValue()) {
+         return -1;
+      }
+      else if(this.getValue() < ((Node) arg0).getValue()) {
+         return 1;
+      }
+      return 0;
+   }
+   
+   public boolean equals(Object x) {
+      if(this.getName().equals(((Node)x).getName())) {
+         return true;
+      }
+      return false;
+   }
+
+   
 }
--- PageRank/src/pageRank.java
+++ PageRank/src/pageRank.java
@@ -2,23 +2,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 
 public class pageRank {
-   private Hashtable<Node, ArrayList<Node>> nodes;
+   private Hashtable<String, ArrayList<Node>> nodes;
+   private ArrayList<Node> ranks;
+   public static int count = 0;
    public static void main(String[] args) {
       if (args.length == 0) {
          pageRank page = new pageRank();
          page.readTextFromFile(new File("data/dolphins.csv"));
          System.out.println(page.getNodes().size());
-        /* Enumeration<Node> e = page.getNodes().keys();
+
+         Enumeration<String> e = page.getNodes().keys();
          while(e.hasMoreElements()) {
-            Node n = e.nextElement();
-            System.out.println(n.getName() + " " +  n.getValue());
-         }*/
+            String n = e.nextElement();
+            page.ranks.add(new Node(n, page.findPageRank(new Node(n, 0.0))));
+            //         System.out.println(page.findPageRank(new Node(n, 0)) + " " + page.nodes.get(n).size() + " " + n);
+            count = 0;
+         }
+         Collections.sort(page.ranks);
+         for(Node n : page.ranks) {
+            System.out.println(n.getName() + " " + n.getValue());
+         }
       } else {
          System.err.println("Usage: HClustering <fileName> [<threshold>]");
          System.exit(1);
@@ -27,10 +37,25 @@ public class pageRank {
    }
 
    public pageRank() {
-      this.nodes = new Hashtable<Node, ArrayList<Node>>();
+      this.nodes = new Hashtable<String, ArrayList<Node>>();
+      this.ranks = new ArrayList<Node>();
    }
 
-   public Hashtable<Node, ArrayList<Node>> getNodes() {
+   public double findPageRank(Node name) {
+      double rank = 0.0;
+      count++;
+      if(count > 1000) {
+         return 0.0;
+      }
+      for(int i = 0; i < nodes.get(name.getName()).size(); i++) { 
+         rank += 1.0/nodes.get(name.getName()).size() * findPageRank(nodes.get(name.getName()).get(i));
+      }
+      rank = rank * .803;
+      rank += (1 - .803) * 1/nodes.size();
+      return rank;
+   }
+
+   public Hashtable<String, ArrayList<Node>> getNodes() {
       return nodes;
    }
    private void readTextFromFile(File file) {
@@ -40,19 +65,45 @@ public class pageRank {
          scanner = new Scanner(file);
          while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
+            line = line.replaceAll("[ ]", "");
             String[] tokens = line.split("[,]");
             a = new Node(tokens[0], Integer.parseInt(tokens[1]));
             b = new Node(tokens[2], Integer.parseInt(tokens[3]));
-            if(!nodes.containsKey(a)) {
-               nodes.put(a, new ArrayList<Node>());
+            if(!nodes.containsKey(a.getName())) {
+               nodes.put(a.getName(), new ArrayList<Node>());
             }
-            if(!nodes.get(a).contains(b))
-               nodes.get(a).add(b);
-            if(!nodes.containsKey(b)) {
-               nodes.put(b, new ArrayList<Node>());
+            if(!nodes.get(a.getName()).contains(b))
+               nodes.get(a.getName()).add(b);
+            if(!nodes.containsKey(b.getName())) {
+               nodes.put(b.getName(), new ArrayList<Node>());
             }
-            if(!nodes.get(b).contains(a))
-               nodes.get(b).add(a);
+            if(!nodes.get(b.getName()).contains(a))
+               nodes.get(b.getName()).add(a);
+         }
+      }
+      catch (FileNotFoundException e) {
+         System.err.println("FILE: " + file.getName() + " NOT FOUND");
+         e.printStackTrace();
+      }
+   }
+
+   private void readDirectedTextFromFile(File file) {
+      Scanner scanner;
+      Node a, b;
+      try {
+         scanner = new Scanner(file);
+         while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line = line.replaceAll("[ ]", "");
+            String[] tokens = line.split("[,]");
+            a = new Node(tokens[0], Integer.parseInt(tokens[1]));
+            b = new Node(tokens[2], Integer.parseInt(tokens[3]));
+
+            if(!nodes.containsKey(a.getName())) {
+               nodes.put(a.getName(), new ArrayList<Node>());
+            }
+            if(!nodes.get(a.getName()).contains(b))
+               nodes.get(a.getName()).add(b);
          }
       }
       catch (FileNotFoundException e) {
